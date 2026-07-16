a5b89a695bc1ee76eefe4d179b0331d622fed308
Ader De Souza <aderdesouza@Aders-Mac-Studio.local>
2026-07-16 03:38:03 -0400

    Route audio smoke through worker API

--- python_packages/reel_factory/reel_factory/worker_api.py
+++ python_packages/reel_factory/reel_factory/worker_api.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from .audio_intent import read_audio_intent
 from .caption_bank import CaptionBankStore, load_or_build_caption_bank_store
 from .reference_video_remix import (
     build_reference_video_remix_plan,
@@ -13,4 +14,5 @@ __all__ = [
     "build_reference_video_remix_plan",
     "gemini_motion_analysis_instruction",
     "load_or_build_caption_bank_store",
+    "read_audio_intent",
 ]
